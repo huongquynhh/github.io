9922415fd57ad803011981c0d12501287c0d79c2
huongquynhh <101454449+huongquynhh@users.noreply.github.com>
2022-05-08 22:29:18 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_thaydoimaunen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void hsbRed_ValueChanged(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(hsbRed.Value, hsbGreen.Value, hsbBlue.Value);
+            lblRed.Text = hsbRed.Value.ToString();
+
+        }
+
+        private void hsbGreen_ValueChanged(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(hsbRed.Value, hsbGreen.Value, hsbBlue.Value);
+            lblGreen.Text = hsbGreen.Value.ToString();
+
+        }
+
+        private void hsbBlue_ValueChanged(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(hsbRed.Value, hsbGreen.Value, hsbBlue.Value);
+            lblBlue.Text = hsbBlue.Value.ToString();
+
+        }
+    }
+}

8902c7f7812d992bdf79f9403830660e8b179f2f
huongquynhh <101454449+huongquynhh@users.noreply.github.com>
2022-05-08 22:31:32 +0700

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace form_thaydoimaunen
+namespace form_ptrinhbachai
 {
     public partial class Form1 : Form
     {
@@ -17,25 +17,91 @@ namespace form_thaydoimaunen
             InitializeComponent();
         }
 
-        private void hsbRed_ValueChanged(object sender, EventArgs e)
+        private void btnGiaiPTBH_Click(object sender, EventArgs e)
         {
-            this.BackColor = Color.FromArgb(hsbRed.Value, hsbGreen.Value, hsbBlue.Value);
-            lblRed.Text = hsbRed.Value.ToString();
+            PTBH(Convert.ToInt32(nudNhapA.Value), Convert.ToInt32(nudNhapB.Value),
+Convert.ToInt32(nudNhapC.Value));
+        }
+
+        private void PTBH(int a, int b, int c)
+        {
+            double delta, x1, x2;
+            if ((a == 0) || (b == 0) || (c == 0))
+            {
+                txtKetqua.Text = "Không giải vì không phải phương trình bậc hai đủ";
+                btnGiaiPTBH.Enabled = false;
+                return;
+            }
+            delta = b * b - 4 * a * c;
+            if (delta < 0)
+            {
+                txtKetqua.Text = txtKetqua.Text + "Phương trình vô nghiệm";
+                btnGiaiPTBH.Enabled = false;
+                return;
+            }
+            if (delta == 0)
+            {
+                x1 = Math.Round((-b / (2.0 * a)), 2);
+                txtKetqua.Text = txtKetqua.Text + "Phương trình có nghiệm kép";
+                txtX1.Text = Convert.ToString(x1);
+                txtX2.Text = Convert.ToString(x1);
+                btnGiaiPTBH.Enabled = false;
+                return;
+            }
+            x1 = Math.Round((-b + Math.Sqrt(delta)) / (2 * a), 2);
+            x2 = Math.Round((-b - Math.Sqrt(delta)) / (2 * a), 2);
+            txtKetqua.Text = txtKetqua.Text + "Phương trình có 2 nghiệm phân biệt";
+            txtX1.Text = Convert.ToString(x1);
+            txtX2.Text = Convert.ToString(x2);
+            btnGiaiPTBH.Enabled = false;
+        }
+
+        private void EmptyText()
+        {
+            txtX1.Text = "";
+            txtX2.Text = "";
+        }
+
+        private void nudNhapA_ValueChanged(object sender, EventArgs e)
+        {
+            txtKetqua.Text = "A=" + nudNhapA.Value + System.Environment.NewLine;
+            txtKetqua.Text = txtKetqua.Text + "B=" + nudNhapB.Value +
+System.Environment.NewLine;
+            txtKetqua.Text = txtKetqua.Text + "C=" + nudNhapC.Value +
+System.Environment.NewLine;
+            btnGiaiPTBH.Enabled = true;
+            EmptyText();
 
         }
 
-        private void hsbGreen_ValueChanged(object sender, EventArgs e)
+        private void nudNhapB_ValueChanged(object sender, EventArgs e)
         {
-            this.BackColor = Color.FromArgb(hsbRed.Value, hsbGreen.Value, hsbBlue.Value);
-            lblGreen.Text = hsbGreen.Value.ToString();
+            txtKetqua.Text = "A=" + nudNhapA.Value + System.Environment.NewLine;
+            txtKetqua.Text = txtKetqua.Text + "B=" + nudNhapB.Value +
+System.Environment.NewLine;
+            txtKetqua.Text = txtKetqua.Text + "C=" + nudNhapC.Value +
+System.Environment.NewLine;
+            btnGiaiPTBH.Enabled = true;
+            EmptyText();
 
         }
 
-        private void hsbBlue_ValueChanged(object sender, EventArgs e)
+        private void nudNhapC_ValueChanged(object sender, EventArgs e)
         {
-            this.BackColor = Color.FromArgb(hsbRed.Value, hsbGreen.Value, hsbBlue.Value);
-            lblBlue.Text = hsbBlue.Value.ToString();
+            txtKetqua.Text = "A=" + nudNhapA.Value + System.Environment.NewLine;
+            txtKetqua.Text = txtKetqua.Text + "B=" + nudNhapB.Value +
+System.Environment.NewLine;
+            txtKetqua.Text = txtKetqua.Text + "C=" + nudNhapC.Value +
+System.Environment.NewLine;
+            btnGiaiPTBH.Enabled = true;
+            EmptyText();
 
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
+

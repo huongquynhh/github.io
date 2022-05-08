57c3ddf06636fed73b31b95520a6820a39a5a1d9
huongquynhh <101454449+huongquynhh@users.noreply.github.com>
2022-05-08 22:30:09 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace form_thaydoimaunen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hsbRed = new System.Windows.Forms.HScrollBar();
+            this.hsbGreen = new System.Windows.Forms.HScrollBar();
+            this.hsbBlue = new System.Windows.Forms.HScrollBar();
+            this.lblRed = new System.Windows.Forms.Label();
+            this.lblGreen = new System.Windows.Forms.Label();
+            this.lblBlue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // hsbRed
+            // 
+            this.hsbRed.Location = new System.Drawing.Point(230, 249);
+            this.hsbRed.Maximum = 255;
+            this.hsbRed.Name = "hsbRed";
+            this.hsbRed.Size = new System.Drawing.Size(216, 31);
+            this.hsbRed.TabIndex = 0;
+            this.hsbRed.ValueChanged += new System.EventHandler(this.hsbRed_ValueChanged);
+            // 
+            // hsbGreen
+            // 
+            this.hsbGreen.Location = new System.Drawing.Point(230, 296);
+            this.hsbGreen.Maximum = 255;
+            this.hsbGreen.Name = "hsbGreen";
+            this.hsbGreen.Size = new System.Drawing.Size(216, 26);
+            this.hsbGreen.TabIndex = 1;
+            this.hsbGreen.ValueChanged += new System.EventHandler(this.hsbGreen_ValueChanged);
+            // 
+            // hsbBlue
+            // 
+            this.hsbBlue.Location = new System.Drawing.Point(230, 343);
+            this.hsbBlue.Maximum = 255;
+            this.hsbBlue.Name = "hsbBlue";
+            this.hsbBlue.Size = new System.Drawing.Size(216, 27);
+            this.hsbBlue.TabIndex = 2;
+            this.hsbBlue.ValueChanged += new System.EventHandler(this.hsbBlue_ValueChanged);
+            // 
+            // lblRed
+            // 
+            this.lblRed.AutoSize = true;
+            this.lblRed.Location = new System.Drawing.Point(137, 266);
+            this.lblRed.Name = "lblRed";
+            this.lblRed.Size = new System.Drawing.Size(27, 13);
+            this.lblRed.TabIndex = 3;
+            this.lblRed.Text = "Red";
+            // 
+            // lblGreen
+            // 
+            this.lblGreen.AutoSize = true;
+            this.lblGreen.Location = new System.Drawing.Point(140, 308);
+            this.lblGreen.Name = "lblGreen";
+            this.lblGreen.Size = new System.Drawing.Size(36, 13);
+            this.lblGreen.TabIndex = 4;
+            this.lblGreen.Text = "Green";
+            // 
+            // lblBlue
+            // 
+            this.lblBlue.AutoSize = true;
+            this.lblBlue.Location = new System.Drawing.Point(140, 356);
+            this.lblBlue.Name = "lblBlue";
+            this.lblBlue.Size = new System.Drawing.Size(28, 13);
+            this.lblBlue.TabIndex = 5;
+            this.lblBlue.Text = "Blue";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblBlue);
+            this.Controls.Add(this.lblGreen);
+            this.Controls.Add(this.lblRed);
+            this.Controls.Add(this.hsbBlue);
+            this.Controls.Add(this.hsbGreen);
+            this.Controls.Add(this.hsbRed);
+            this.Name = "Form1";
+            this.Text = "Màu nền form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.HScrollBar hsbRed;
+        private System.Windows.Forms.HScrollBar hsbGreen;
+        private System.Windows.Forms.HScrollBar hsbBlue;
+        private System.Windows.Forms.Label lblRed;
+        private System.Windows.Forms.Label lblGreen;
+        private System.Windows.Forms.Label lblBlue;
+    }
+}
+
